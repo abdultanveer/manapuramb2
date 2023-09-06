74731cce191abad6a26c10096dc81f0ac7ab47de
Abdul <abdul.tanveer@gmail.com>
2023-09-06 14:20:46 +0530

    stateful widget

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar( title: Text("welcome to flutter"),),
-    body: Center(child: Text("you clicked 12 times")),
-    floatingActionButton: FloatingActionButton(
-      onPressed: _increment,
-      child: Icon(Icons.add),
-    ),
-  ),
+  home: MyText()
 ));
 
 
-void _increment() {
+class MyText extends StatefulWidget {
+  const MyText({super.key});
+
+  @override
+  State<MyText> createState() => _MyTextState();
 }
+
+class _MyTextState extends State<MyText> {
+  int counter = 0;
+  void _increment(){
+    setState(() {
+      counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( title: Text("welcome to flutter"),),
+      body: Center(child: Text("you clicked $counter times")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _increment,
+        child: Icon(Icons.add),
+      ),
+    );
+
+  }
+}
+
+
+
