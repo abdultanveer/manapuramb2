f6e3a90f30c339537c522aed701018c2fed8988d
Abdul <abdul.tanveer@gmail.com>
2023-09-09 16:49:09 +0530

    List<Album> fixed init

--- lib/dartexs/async/test_json.dart
+++ lib/dartexs/async/test_json.dart
@@ -3,7 +3,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:manapuramb2/model/album.dart';
 void main()async {
- var album = await fetchAlbum();
+ var albumlist = await fetchAlbum();
+ print(albumlist);
 }
 
 Future<http.Response> getJson() async {
@@ -11,15 +12,16 @@ Future<http.Response> getJson() async {
   return response;
 }
 
-Future<Album> fetchAlbum() async {
+Future<List<Album>> fetchAlbum() async {
   http.Response response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
   List<dynamic> albums = jsonDecode(response.body);
   var album = Album.fromJson(albums[2]);
   print(album.title);
+  List<Album> albumList = [];
   for (int i=0;i<albums.length; i++){
-    //List<Album> albumList =  albumList.add(Album.fromJson(albums[i]));
+     albumList.add(Album.fromJson(albums[0]));
   }
   //return a List<Album> albums
   //return Album(userId: 1, id: 2, title: "ansari");
-  return album;
+  return albumList;
 }
