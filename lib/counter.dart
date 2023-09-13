01156b05faa7d6f9d618d8b6566b5cd3c1ab7dab
Abdul <abdul.tanveer@gmail.com>
2023-09-13 10:05:24 +0530

    dependencies n fetchalbum

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,7 +1,33 @@
+import 'package:manapuramb2/model/album.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
+
 class Counter {
   int value = 0;
 
   void increment() => value++;
 
   void decrement() => value--;
+}
+
+/*void main()async{
+  fetchAlbum(http.Client());
+}*/
+
+Future<Album> fetchAlbum(http.Client client) async {
+  final response = await client
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    print(response.body);
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
 }
--- test/counter_test.dart
+++ test/counter_test.dart
@@ -5,8 +5,30 @@ import '../lib/counter.dart';
 main(){
   Counter counter = Counter();
   counter.increment();
-  int expected = 2;
+  int expected = 1;
   int actual = counter.value;
   test('test increment method', () => expect(actual,expected));
+
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+
+    test('value should be incremented', () {
+      final counter = Counter();
+
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
+  });
 
 }
