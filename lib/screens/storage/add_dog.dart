c29f2a0842234defed8a9afafa29cde387e0fdef
Abdul <abdul.tanveer@gmail.com>
2023-09-13 11:55:42 +0530

    add dog screen

---
+++ lib/screens/storage/add_dog.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:manapuramb2/database/crud.dart';
+import 'package:manapuramb2/model/dog.dart';
+import 'package:manapuramb2/screens/storage/dog_screen.dart';
+
+class AddDog extends StatelessWidget {
+   AddDog(DogDao dogDao, {super.key});
+  var idController = TextEditingController();
+  var nameController = TextEditingController();
+  var ageController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          TextField(controller: idController ,decoration: InputDecoration(labelText: "enter id"),),
+          TextField(controller:nameController ,decoration: InputDecoration(labelText: "enter name"),),
+          TextField(controller: ageController ,decoration: InputDecoration(labelText: "enter age"),),
+          ElevatedButton(onPressed: (){
+           addDog();
+           Navigator.pop(context);
+          }, child: Text('save'))
+        ],
+      ),
+    );
+  }
+
+   void addDog() async {
+      var dog = Dog(id: int.parse(idController.text),
+        name: nameController.text,
+        age: int.parse(ageController.text));
+    await dogDao.insertDog(dog)    ;
+   }
+}
--- lib/screens/storage/dog_screen.dart
+++ lib/screens/storage/dog_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:manapuramb2/database/crud.dart';
 import 'package:manapuramb2/model/dog.dart';
+import 'package:manapuramb2/screens/storage/add_dog.dart';
 
 
 var dogDao = new DogDao();
 var database;
-void main() async{
+
+void main() async {
   database = await dogDao.openDb();
   runApp(MaterialApp(
     home: DogsApp(),
@@ -14,9 +16,7 @@ void main() async{
 
 class DogsApp extends StatelessWidget {
   DogsApp({super.key});
-  var idController = TextEditingController();
-  var nameController = TextEditingController();
-  var ageController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -25,16 +25,14 @@ class DogsApp extends StatelessWidget {
       body: Column(
         mainAxisSize: MainAxisSize.max,
         children: [
-          TextField(controller: idController ,decoration: InputDecoration(labelText: "enter id"),),
-          TextField(controller:nameController ,decoration: InputDecoration(labelText: "enter name"),),
-          TextField(controller: ageController ,decoration: InputDecoration(labelText: "enter age"),),
-          FutureBuilder(future: getAllDogs(), builder: (context,snapshot){
+
+          FutureBuilder(future: getAllDogs(), builder: (context, snapshot) {
             return Flexible(
               flex: 1,
               child: Center(
                 child: ListView.builder(
                     itemCount: snapshot.data!.length,
-                    itemBuilder: (context,index){
+                    itemBuilder: (context, index) {
                       return ListTile(
                         leading: Text(snapshot.data![index].age.toString()),
                         title: Text(snapshot.data![index].name),
@@ -48,20 +46,25 @@ class DogsApp extends StatelessWidget {
 
 
       floatingActionButton: FloatingActionButton(
-        onPressed: addDog ,
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context)=>  AddDog(dogDao)));
+        },
         child: Icon(Icons.add),
       ),
     );
   }
 
   void addDog() async {
-    var dog = Dog(id: int.parse(idController.text),
+    /* var dog = Dog(id: int.parse(idController.text),
         name: nameController.text,
         age: int.parse(ageController.text));
-    await dogDao.insertDog(dog)    ;
+    await dogDao.insertDog(dog)    ;*/
   }
 
-  Future<List<Dog>> getAllDogs(){
+  Future<List<Dog>> getAllDogs() {
     return dogDao.readDog();
   }
+
+
 }
